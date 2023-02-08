4d79874b685411f5d5ce8957b78ed5ec9d728487
ericdequ <dequevedo.eric@ufl.edu>
2023-02-08 13:22:46 -0500

    Added Signup functionality

---
+++ my-app/src/firebase/firebase.tsx
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import {getAuth} from '@firebase/auth'
+import { getFirestore } from "@firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAn5rjr-x7S6q5qs-g6IkyF9dHQgTNscYs",
+  authDomain: "freel-ee39b.firebaseapp.com",
+  projectId: "freel-ee39b",
+  storageBucket: "freel-ee39b.appspot.com",
+  messagingSenderId: "828543545656",
+  appId: "1:828543545656:web:c692555e6b8d49c30d11cd",
+  measurementId: "G-6KVJXG35XJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+
+
+export {app,auth,db};
--- my-app/src/pages/screens/test.tsx
+++ my-app/src/pages/screens/test.tsx
@@ -8,7 +8,7 @@ function Testr() {
             <Link href="./screens/Login">
                 <img src="./images/Logo.png" alt="" width="249" height="107"/>
             </Link>
-            <h1>YO</h1>
+            <h1>Signed in</h1>
         </div>
         <div></div>
     </header>
