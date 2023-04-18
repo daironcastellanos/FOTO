a8678e9ee6c1c7e8d041a58274bff41a8500574e
Eric deQuevedo <ericdequevedo@gmail.com>
2023-04-17 20:58:29 -0400

    changes

--- my-app/src/firebase/firebase.tsx
+++ my-app/src/firebase/firebase.tsx
@@ -4,8 +4,6 @@ import { getFirestore } from "@firebase/firestore";
 import { useState, useEffect, useContext, createContext } from 'react'
 import { GoogleAuthProvider } from "firebase/auth";
 
-
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
