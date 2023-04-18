8e032a661e8949efcd8e4c19c3b6bc92f9307d3a
Sergio Arcila <SergioArcila@ufl.edu>
2023-04-17 20:49:14 -0400

    Changed the header

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -5,16 +5,15 @@ import logo from '../../public/images/Logo.png';
 
 type HeaderProps = {
   isLoggedIn: boolean;
-}
+};
 
 const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
   e.preventDefault();
   try {
-    if (sessionStorage.getItem("loggedIn") != null) {
-      sessionStorage.removeItem("loggedIn");
+    if (sessionStorage.getItem('loggedIn') != null) {
+      sessionStorage.removeItem('loggedIn');
       window.location.reload();
-    }
-    else {
+    } else {
       console.log('user has not signed in.');
     }
   } catch (error) {
@@ -25,103 +24,102 @@ const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 const Header = ({ isLoggedIn }: HeaderProps) => {
   return (
     <div>
-      {isLoggedIn ? (
-        <SignoutHeader/>
-      ) : (
-        <SigninHeader/>
-      )}
+      {isLoggedIn ? <SignoutHeader /> : <SigninHeader />}
     </div>
   );
 };
 
 const SigninHeader = () => {
   return (
-    <header className='flex justify-between p-1 max-w-7xl mx-auto'>
-      <div className='flex items-center space-x-5'>
-        <Link href="/">
-          <div className="w-44">
-            <Image
-              className="object-contain"
-              src={logo.src}
-              alt="Logo"
-              width={200}
-              height={50}
-              layout="responsive"
-            />
+    <header className="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 sticky top-0 z-10">
+      <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-20">
+          <div className="flex items-center">
+            <Link href="/">
+              <div className="w-20 cursor-pointer">
+                <Image
+                  className="object-contain"
+                  src={logo.src}
+                  alt="Logo"
+                  width={150}
+                  height={40}
+                  layout="responsive"
+                />
+              </div>
+            </Link>
           </div>
-        </Link>
-      </div>
-      <div className='hidden md:inline-flex items-center space-x-5'>
-      <Link href="/">
-            <h3 className='text-lg'>Home</h3>
+          <div className="hidden md:block">
+            <div className="ml-10 flex items-baseline space-x-4">
+              <Link href="/">
+                <span className="text-white text-xl font-semibold hover:text-blue-300 cursor-pointer">Home</span>
+              </Link>
+              <Link href="/features">
+                <span className="text-white text-xl font-semibold hover:text-blue-300 cursor-pointer">Features</span>
+              </Link>
+              <Link href="/about">
+                <span className="text-white text-xl font-semibold hover:text-blue-300 cursor-pointer">About</span>
+              </Link>
+              <Link href="/follow">
+                <span className="text-white text-xl font-semibold hover:text-blue-300 cursor-pointer">Follow</span>
+              </Link>
+            </div>
+          </div>
+          <div className="md:flex items-center justify-end md:flex-1 lg:w-0">
+            <Link href="/screens/Login">
+              <span className="whitespace-nowrap text-xl font-semibold text-white hover:text-blue-300 cursor-pointer">
+                Sign in
+              </span>
             </Link>
-        <Link href="/features">
-            <h3 className='text-lg'>Features</h3>
+            <Link href="/screens/SignUpForm">
+              <span className="whitespace-nowrap ml-4 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-xl font-semibold text-white bg-yellow-400 hover:bg-yellow-500 cursor-pointer">
+                Get started
+              </span>
             </Link>
-            <Link href="/about">
-            <h3 className='text-lg'>About</h3>
-            </Link>
-            <Link href="/follow">
-            <h3 className='text-lg'>Follow</h3>
-            </Link>
+          </div>
         </div>
-    
-      <div className='flex items-center space-x-5 text-green-600'> 
-      <Link href="/screens/Login">
-        <h3>Sign in</h3>
-        </Link>
-        <Link href="/screens/SignUpForm">
-        <h3 className='text-white bg-blue-600 px-4 py-3 rounded-full'>Get started</h3>
-        </Link>
-      </div>
+      </nav>
     </header>
   );
-}
+};
+
 
 const SignoutHeader = () => {
   return (
-    <header className='flex justify-between p-2 max-w-7xl mx-auto'>
-      <div className='flex items-center space-x-5'>
+    <header className="bg-white py-4 px-4 sm:px-6 lg:px-8 shadow-md">
+      <nav className="relative flex items-center justify-between">
         <Link href="/">
-          <div className="w-44">
-            <Image
-              className="object-contain"
-              src={logo.src}
-              alt="Logo"
-              width={200}
-              height={50}
-              layout="responsive"
-            />
-          </div>
+          <a>
+            <Image src={logo.src} alt="Logo" width={200} height={50} layout="intrinsic" />
+          </a>
         </Link>
-      </div>
-      <div className='hidden md:inline-flex items-center space-x-5'>
-      <Link href="/">
-            <h3 className='text-lg'>Home</h3>
-            </Link>
-        <Link href="/features">
-            <h3 className='text-lg'>Features</h3>
-            </Link>
-            <Link href="/about">
-            <h3 className='text-lg'>About</h3>
-            </Link>
-            <Link href="/follow">
-            <h3 className='text-lg'>Follow</h3>
-            </Link>
-        </div>
-      
-      <div className='flex items-center space-x-5 text-green-600'> 
-        <form onSubmit={handleSubmit}>
-          <button 
-            className='text-white bg-blue-600 px-4 py-3 rounded-full'
-            type='submit'
-          >
-            Sign out
-          </button>
-        </form>
-      </div>
-    </header>
-  );
-}
+        <div className="hidden md:flex space-x-10">
+          <Link href="/">
+            <a className="text-gray-700 font-medium hover:text-blue-500">Home</a>
+          </Link>
+          <Link href="/features">
+            <a className="text-gray-700 font-medium hover:text-blue-500">Features</a>
+          </Link>
+          <Link href="/about">
+            <a className="text-gray-700 font-medium hover:text-blue-500">
+            About</a>
+</Link>
+<Link href="/follow">
+<a className="text-gray-700 font-medium hover:text-blue-500">Follow</a>
+</Link>
+</div>
+<div className="md:flex items-center justify-end md:flex-1 lg:w-0">
+<form onSubmit={handleSubmit}>
+<button
+           className="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-700"
+           type="submit"
+         >
+Sign out
+</button>
+</form>
+</div>
+</nav>
+</header>
+);
+};
 
 export default Header;
