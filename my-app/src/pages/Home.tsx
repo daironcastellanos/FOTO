3ce7ff2c498263a63f9b3a21a4bba45a91ed1638
Eric deQuevedo <ericdequevedo@gmail.com>
2023-04-19 15:50:18 -0400

    Mobile responsive PRoduction Home

--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -1,28 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import PhotoFeed from '@/components/Photofeed';
 import ScrollingView from '@/components/ScrollView';
 import Menu from '@/components/Menu';
 import SuggestedFollowers from '@/components/SuggestedFollowers';
 import Header from '@/components/Header';
-
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Home: React.FC = () => {
+  const [isClient, setIsClient] = useState(false);
+  const [showSuggestedFollowers, setShowSuggestedFollowers] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const handleHamburgerClick = () => {
+    setShowSuggestedFollowers(!showSuggestedFollowers);
+  };
+
   return (
     <div className="relative grid grid-cols-12 gap-4">
       {/* Left column: Menu */}
-      <Menu/>
+      <Menu />
 
       {/* Middle column: Feed */}
-      <div className="col-span-6 col-start-4 bg-white p-4 max-h-screen">
+      <div
+        className={`${
+          showSuggestedFollowers || typeof window === 'undefined' ? 'col-span-6' : 'col-span-9'
+        } ${
+          showSuggestedFollowers || typeof window === 'undefined' ? 'col-start-4' : 'col-start-3'
+        } bg-white p-4 max-h-screen`}
+      >
         <h3 className="text-gray-800 font-bold mb-4 text-center">Photography Feed</h3>
-      <div className="col-span-6 col-start-4 bg-white p-4 max-h-screen overflow-y-auto">
-        <ScrollingView/>
-      </div>
+        <div className="bg-white p-4 max-h-screen overflow-y-auto">
+          <ScrollingView />
+        </div>
       </div>
 
       {/* Right column: Suggested followers */}
-      <SuggestedFollowers/>
+      {isClient && (
+        <div
+          className={`${
+            showSuggestedFollowers || typeof window === 'undefined'
+              ? 'block'
+              : 'hidden'
+          } md:block col-span-3 bg-gray-100 p-4 min-h-screen border-l border-gray-300`}
+        >
+          {typeof window !== 'undefined' && (
+            <button
+              className="hamburger p-1 mb-4 bg-blue-600 text-white rounded flex items-center justify-center"
+              onClick={handleHamburgerClick}
+            >
+              {showSuggestedFollowers ? (
+                <span className="mr-2">Close</span>
+              ) : (
+                <GiHamburgerMenu className="text-2xl" />
+              )}
+            </button>
+          )}
+          <SuggestedFollowers />
+        </div>
+      )}
     </div>
   );
 };
--- my-app/src/pages/upload.tsx
+++ my-app/src/pages/upload.tsx
@@ -1,21 +1,34 @@
-import React, { useState } from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { auth } from "@/firebase/firebase";
+
+const getUser = async () => {
+  const user = auth.currentUser;
+  console.log(user?.uid);
+  console.log(user?.email);
+
+  return user;
+};
 
 interface UploadPageProps {
   onSubmit: (imageUrl: string) => void;
 }
 
 const UploadPage: React.FC<UploadPageProps> = ({ onSubmit }) => {
-  const [imageUrl, setImageUrl] = useState('');
+  const [uploadedFile, setUploadedFile] = useState<File | null>(null);
+  const [imageUrl, setImageUrl] = useState("");
 
-  const handleFileInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     const file = event.target.files?.[0];
     if (file) {
+      setUploadedFile(file);
       const reader = new FileReader();
       reader.onload = () => {
         const result = reader.result;
-        if (typeof result === 'string') {
+        if (typeof result === "string") {
           setImageUrl(result);
         }
       };
@@ -23,40 +36,141 @@ const UploadPage: React.FC<UploadPageProps> = ({ onSubmit }) => {
     }
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handlePhotoUpload = async (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
     event.preventDefault();
-    onSubmit(imageUrl);
+    if (uploadedFile) {
+      await uploadPhoto();
+    }
+  };
+
+  const handleProfilePhotoUpload = async (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+    if (uploadedFile) {
+      await uploadProfilePhoto();
+    }
+  };
+
+  const uploadPhoto = async () => {
+    if (!uploadedFile) {
+      console.error("No file selected for upload");
+      return;
+    } else {
+      const user = await getUser();
+      const fireID = user?.uid;
+
+      const formData = new FormData();
+      formData.append("file", uploadedFile);
+
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/upload/${fireID}`,
+          {
+            method: "POST",
+            body: formData,
+          }
+        );
+        if (
+          response.ok &&
+          response.headers.get("Content-Type")?.includes("application/json")
+        ) {
+          const data = await response.json();
+          console.log(data);
+        } else {
+          console.error("Error uploading photo:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Error uploading photo:", error);
+      }
+    }
+  };
+
+  const uploadProfilePhoto = async () => {
+    if (!uploadedFile) {
+      console.error("No file selected for upload");
+      return;
+    } else {
+      const user = await getUser();
+      const fireID = user?.uid;
+
+      const formData = new FormData();
+      formData.append("file", uploadedFile);
+
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/users/${fireID}/uploadProfilePicture`,
+          {
+            method: "POST",
+            body: formData,
+          }
+        );
+        if (
+          response.ok &&
+          response.headers.get("Content-Type")?.includes("application/json")
+        ) {
+          const data = await response.json();
+          console.log(data);
+        } else {
+          console.error("Error uploading photo:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Error uploading photo:", error);
+      }
+    }
   };
 
   return (
     <div className="max-w-xl mx-auto p-4">
-         <Link href="/Home">
-            <h1 className="absolute top-3 left-3 bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-600">
-              Back
-            </h1>
-          </Link>
+      <Link href="/Home">
+        <h1 className="absolute top-3 left-3 bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-600">
+          Back
+        </h1>
+      </Link>
       <h1 className="text-3xl font-bold mb-4">Upload an Image</h1>
-      <form onSubmit={handleSubmit} className="bg-white rounded-lg shadow-md p-4">
+      <form className="bg-white rounded-lg shadow-md p-4">
         <div className="mb-4">
           <label htmlFor="file-input" className="block font-medium mb-1">
             Choose a file:
           </label>
-          <input type="file" id="file-input" onChange={handleFileInputChange} className="border rounded-lg p-2" />
+          <input
+            type="file"
+            id="file-input"
+            onChange={handleFileInputChange}
+            className="border rounded-lg p-2"
+          />
         </div>
         {imageUrl && (
           <div className="mb-4">
-            <Image src={imageUrl} alt="Preview" className="max-w-full h-auto" />
+            <Image
+              src={imageUrl}
+              alt="Preview"
+              className="max-w-full h-auto"
+              width={640}
+              height={480}
+              layout="responsive"
+              objectFit="contain"
+            />
           </div>
         )}
-        <div className="text-center">
+        <div className="text-center mb-4">
           <button
-            type="submit"
+            onClick={handlePhotoUpload}
             disabled={!imageUrl}
-            className={`bg-blue-500 text-white rounded-lg font-medium py-2 px-4 ${
-              !imageUrl ? 'opacity-50 cursor-not-allowed' : ''
-            }`}
+            className={imageUrl ? "btn" : "btn-disabled"}
           >
             Upload
+          </button>
+        </div>
+        <div className="text-center">
+          <button
+            onClick={handleProfilePhotoUpload}
+            disabled={!imageUrl}
+            className={imageUrl ? "btn" : "btn-disabled"}
+          >
+            Upload Profile Picture
           </button>
         </div>
       </form>
